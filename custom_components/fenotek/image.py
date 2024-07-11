dfc206aa25fd5e9caf7588c997bff28283e9adb0
Thibault Cohen <titilambert@ttb.lt>
2024-07-11 02:17:25 -0400

    Improve sensors

--- custom_components/fenotek/image.py
+++ custom_components/fenotek/image.py
@@ -34,8 +34,6 @@ IMAGE_TYPE = ImageEntityDescription(  # type: ignore[call-arg]
 class FenotekImage(CoordinatorEntity, ImageEntity):
     """Implementation of an Environment Canada radar camera."""
 
-    # _attr_has_entity_name = True
-
     def __init__(
         self,
         coordinator: FenotekDataUpdateCoordinator,
@@ -47,13 +45,9 @@ class FenotekImage(CoordinatorEntity, ImageEntity):
         ImageEntity.__init__(self, hass)
         self._doorbell = doorbell
 
-        # self.radar_object = coordinator.ec_data
-        self._attr_unique_id = f"{doorbell.id_}-camera"
-        # self._attr_attribution = self.radar_object.metadata["attribution"]
-        # self._attr_entity_registry_enabled_default = False
+        self._attr_unique_id = f"{doorbell.id_}-ring"
 
         device_info = DeviceInfo(
-            # config_entry_id=coordinator.config_entry.entry_id,
             connections=doorbell.connections,
             identifiers={(DOMAIN, doorbell.identifiers)},
             name=doorbell.name,
@@ -64,11 +58,11 @@ class FenotekImage(CoordinatorEntity, ImageEntity):
         )
 
         self._attr_device_info = device_info
-        self._attr_image_url = self._doorbell.last_notification_url
-        self._attr_image_last_updated = self._doorbell.last_notification_date
+        if self._doorbell.last_ring:
+            self._attr_image_url = self._doorbell.last_ring.url
+            self._attr_image_last_updated = self._doorbell.last_ring.created_at
         self._attr_content_type = "image/jpeg"
         self.entity_description = IMAGE_TYPE
-        # self._attr_name
 
     @property
     def available(self) -> bool:
@@ -77,8 +71,12 @@ class FenotekImage(CoordinatorEntity, ImageEntity):
 
     @callback
     def _handle_coordinator_update(self) -> None:
-        if self._attr_image_url != self._doorbell.last_notification_url:
-            self._attr_image_url = self._doorbell.last_notification_url
-            self._attr_image_last_updated = self._doorbell.last_notification_date
+        if (
+            self._doorbell.last_ring
+            and self._attr_image_url != self._doorbell.last_ring.url
+        ):
+            self._attr_image_url = self._doorbell.last_ring.url
+            self._attr_image_last_updated = self._doorbell.last_ring.created_at
             self._cached_image = None
+        self.async_write_ha_state()
         super()._handle_coordinator_update()
